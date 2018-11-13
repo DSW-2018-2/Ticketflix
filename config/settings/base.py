ee0df7463df87b6159a6d52a7ecfada0014b52ee
Natália Rodrigues <nataliamrq@gmail.com>
2018-11-13 16:51:04 -0200

    Creating Bomboniere App
    Adds models of Bomboniere App
    Applies Composite Pattern in models
    
    Signed-off-by: Natália Rodrigues <nataliamrq@gmail.com>

--- config/settings/base.py
+++ config/settings/base.py
@@ -70,6 +70,7 @@ THIRD_PARTY_APPS = [
 ]
 LOCAL_APPS = [
     'ticketflix.users.apps.UsersAppConfig',
+    'ticketflix.bomboniere.apps.BomboniereAppConfig'
     # Your stuff: custom apps go here
 ]
 # https://docs.djangoproject.com/en/dev/ref/settings/#installed-apps
