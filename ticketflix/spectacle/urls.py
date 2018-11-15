2624aaa87f176d078e805661ced58a32f44fc3ee
João Pedro Sconetto <sconetto.joao@gmail.com>
2018-11-15 19:09:40 -0200

    [WIP] Creating Movie Form
    Separating spectacle and movie forms
    
    Adding movie create view
    
    Adding decorators URL
    
    Removing old forms.py
    
    Co-authored-by: Mariana Mendes <mare.s.mendes@gmail.com>

--- ticketflix/spectacle/urls.py
+++ ticketflix/spectacle/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from .views import SpectacleCreateView, SpectacleDeleteView
 from .views import SpectacleListView, SpectacleUpdateView
 from .views import SpectacleDetailView
+from .views import MovieCreateView
 
 
 urlpatterns = [
@@ -11,7 +12,7 @@ urlpatterns = [
         name="spectacle-list"
     ),
     path(
-        "<int:id>",
+        "<int:id>/",
         SpectacleDetailView.as_view(template_name="spectacle/detail.html"),
         name="spectacle-detail"
     ),
@@ -26,8 +27,37 @@ urlpatterns = [
         name="spectacle-update"
     ),
     path(
-        "<int:id>/delete",
+        "<int:id>/delete/",
         SpectacleDeleteView.as_view(template_name="spectacle/delete.html"),
         name="spectacle-delete"
-    )
+    ),
+    path(
+        "movie/create/",
+        MovieCreateView.as_view(template_name="spectacle/movie_form.html"),
+        name="movie-create"
+    ),
+    # path(
+    #     "<int:id>/movie/<int:id>/update"
+    # ),
+    # path(
+    #     "<int:id>/movie/<int:id>/delete/"
+    # ),
+    # path(
+    #     "<int:id>/play/create/"
+    # ),
+    # path(
+    #     "<int:id>/play/<int:id>/update"
+    # ),
+    # path(
+    #     "<int:id>/play/<int:id>/delete/"
+    # ),
+    # path(
+    #     "<int:id>/show/create/"
+    # ),
+    # path(
+    #     "<int:id>/show/<int:id>/update"
+    # ),
+    # path(
+    #     "<int:id>/show/<int:id>/delete/"
+    # ),
 ]
--- ticketflix/spectacle/views.py
+++ ticketflix/spectacle/views.py
@@ -1,7 +1,7 @@
 from django.urls import reverse_lazy
 from django.views.generic import CreateView, DeleteView, ListView, UpdateView, DetailView
 from .models import Spectacle, Movie, Play, Show
-from .forms import SpectacleForm
+
 
 class SpectacleDetailView(DetailView):
     model = Spectacle
@@ -26,7 +26,7 @@ class SpectacleListView(ListView):
 class SpectacleCreateView(CreateView):
     model = Spectacle
     template_name = 'spectacle/form.html'
-    form_class = SpectacleForm
+    fields = '__all__'
 
     def form_valid(self, form):
         return super().form_valid(form)
@@ -75,7 +75,63 @@ class SpectacleUpdateView(UpdateView):
 
 class MovieCreateView(CreateView):
     model = Movie
-    template_name = 'spectacle/form.html'
+    template_name = 'spectacle/movie_form.html'
+    fields = [
+        'synopsis',
+        'diretor',
+        'cast',
+        'producer',
+        'writer',
+        'gender',
+        'trailer',
+    ]
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        spectacles = Spectacle.objects.all()
+        objects = []
+        for spectacle in spectacles:
+            related = Spectacle.objects.get_related_object(spectacle)
+            if related is None:
+                objects.append(spectacle)
+
+        context['spectacles'] = objects
+        return context
+
+    def form_valid(self, form):
+        data = form.data
+        spectacle = Spectacle.objects.get(id=int(data['spectacle']))
+        data['name'] = spectacle.name
+        data['status'] = spectacle.status
+        data['poster'] = spectacle.poster
+        data['duration'] = spectacle.duration
+        data['classification'] = spectacle.classification
+        data['spectacle_type'] = spectacle.spectacle_type
+        data['spectacle_id'] = spectacle.id
+        form.data = data
+        return super().form_valid(form)
+
+    success_url = reverse_lazy(
+        viewname='spectacle:spectacle-list'
+    )
+
+
+class PlayCreateView(CreateView):
+    model = Play
+    template_name = 'spectacle/play_form.html'
+    fields = '__all__'
+
+    def form_valid(self, form):
+        return super().form_valid(form)
+
+    success_url = reverse_lazy(
+        viewname='spectacle:spectacle-list'
+    )
+
+
+class ShowCreateView(CreateView):
+    model = Show
+    template_name = 'spectacle/play_form.html'
     fields = '__all__'
 
     def form_valid(self, form):
