bc6bc4f6d4e63e3983773e8b1be136226fe28681
Andrew Lucas <anlugueso@gmail.com>
2018-11-19 19:55:17 -0200

    Improving Session model
    Signed-off-by: Andrew Lucas <anlugueso@gmail.com>
    Co-authored-by: Maria Luiza <mlfaa@hotmail.com>
    Co-authored-by: Adrianne Alves <driah_as@hotmail.com>

--- ticketflix/session/models.py
+++ ticketflix/session/models.py
@@ -1,13 +1,30 @@
 from django.db import models
 from django.urls import reverse
 
-# Create your models here.
+
 class Session(models.Model):
-    date = models.DateField(auto_now=False, auto_now_add=False)
-    time = models.TimeField( auto_now=False, auto_now_add=False)
-    place = models.CharField( max_length=50)
+
+    date = models.DateField(
+        auto_now=False,
+        auto_now_add=False
+    )
+
+    time = models.TimeField(
+        auto_now=False,
+        auto_now_add=False
+    )
+
+    place = models.CharField(
+        max_length=50
+    )
+
     available_ticket_number = models.IntegerField()
+
     total_ticket_number = models.IntegerField()
 
-    def get_absolute_url(self):
-        return reverse('session-datail', args=[str(self.id)])
+    def __str__(self):
+        return ('Session ' + str(self.id))
+
+    class Meta:
+        verbose_name = ("Sessão")
+        verbose_name_plural = ("Sessões")
