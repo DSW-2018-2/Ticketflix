78461d269eab138d0007f8f77e34a50fbcced740
Andrew Lucas <anlugueso@gmail.com>
2018-11-19 22:03:19 -0200

    Creating Spectacles Schedule page
    Signed-off-by: Andrew Lucas <anlugueso@gmail.com>
    Co-authored-by: Maria Luiza <mlfaa@hotmail.com>
    Co-authored-by: Adrianne Alves <driah_as@hotmail.com>

--- config/urls.py
+++ config/urls.py
@@ -5,8 +5,12 @@ from django.contrib import admin
 from django.views.generic import TemplateView
 from django.views import defaults as default_views
 
+from .views import ScheduleView
+
+
+
 urlpatterns = [
-    path("", TemplateView.as_view(template_name="pages/home.html"), name="home"),
+    path("", ScheduleView.as_view(template_name="pages/home.html"), name="home"),
     path(
         "about/",
         TemplateView.as_view(template_name="pages/about.html"),
